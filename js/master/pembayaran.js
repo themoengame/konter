b0c77e78925782f4be062a879df936883448caf2
themoengame <themoengamecoc@gmail.com>
2026-05-07 15:02:17 +0700

    Create pembayaran.js

---
+++ js/master/pembayaran.js
@@ -0,0 +1,5 @@
+// ==================== MASTER PEMBAYARAN ONLINE ====================
+
+window.renderMasterPembayaran = async function(container) {
+    await renderMaster('pembayaran', container);
+};

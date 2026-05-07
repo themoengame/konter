77a5d4ccabe360584ce27d50b4f765afa6a66005
themoengame <themoengamecoc@gmail.com>
2026-05-07 18:50:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 // config.js
-// Ganti dengan URL Apps Script Anda setelah deploy
-const BASE_URL = 'https://script.google.com/macros/s/AKfycbwJNO7SRTNtoLvyJcTJg0OXHKNayksFyMVvs2_2oblk0OZgd9I3hoVS31msN9vYFV4nXA/exec';
+// URL Web App Google Apps Script
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbwYd-keZ4UidqbCUaDU5NGfMg3UgjJ0AZRoj4Ps0_P3qlhc2qHF_lk3jBm_jIRx9-nm/exec';
 
 // Konfigurasi aplikasi
 const APP_CONFIG = {
-  sessionTimeout: 3600000, // 1 jam dalam milidetik
-  autoSave: true,
-  itemsPerPage: 50
+    sessionTimeout: 3600000, // 1 jam dalam milidetik
+    autoSave: true,
+    itemsPerPage: 50,
+    version: '1.0.0'
 };
+
+// Export untuk penggunaan
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { BASE_URL, APP_CONFIG };
+}

be62921f3ecb6aa644edc141a5ade3a44484c782
themoengame <themoengamecoc@gmail.com>
2026-05-07 17:15:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+// Ganti dengan URL Apps Script Anda setelah deploy
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbwYd-keZ4UidqbCUaDU5NGfMg3UgjJ0AZRoj4Ps0_P3qlhc2qHF_lk3jBm_jIRx9-nm/exec';
+
+// Konfigurasi aplikasi
+const APP_CONFIG = {
+  sessionTimeout: 3600000, // 1 jam dalam milidetik
+  autoSave: true,
+  itemsPerPage: 50
+};

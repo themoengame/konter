3188d36e85a023e0e37ddff4030a4a368c10a5be
themoengame <themoengamecoc@gmail.com>
2026-05-07 17:37:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Ganti dengan URL Apps Script Anda setelah deploy
-const BASE_URL = 'https://script.google.com/macros/s/AKfycbwYd-keZ4UidqbCUaDU5NGfMg3UgjJ0AZRoj4Ps0_P3qlhc2qHF_lk3jBm_jIRx9-nm/exec';
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbwJNO7SRTNtoLvyJcTJg0OXHKNayksFyMVvs2_2oblk0OZgd9I3hoVS31msN9vYFV4nXA/exec';
 
 // Konfigurasi aplikasi
 const APP_CONFIG = {
